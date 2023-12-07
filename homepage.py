2e46596a94cf18e135354e3c72046118fa1b273b
eijane <152346512+eijane@users.noreply.github.com>
2023-12-07 11:15:45 +0800

    Add files via upload

--- homepage.py
+++ homepage.py
@@ -1,22 +1,22 @@
 from PIL import Image
 import requests
 #import streamlit as st
+import streamlit as st
 
-
+st.set_page_config(
+    page_title="Multipage App",
+    page_icon="wave"
+)
 def load_lottieurl(url):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json
 
-st.set_page_config(
-    page_title="Multipage App",
-    page_icon="wave"
-)
+
 
 st.title("Lalay's Blog")
 
-import streamlit as st
 
     # --- LOAD ASSETS ---
 #lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20cfjwiyb.json")
