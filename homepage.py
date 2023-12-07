e0971dd9a1c960a837f74e46e5398b23008d45bf
eijane <152346512+eijane@users.noreply.github.com>
2023-12-07 11:13:41 +0800

    Add files via upload

--- homepage.py
+++ homepage.py
@@ -1,8 +1,7 @@
 from PIL import Image
 import requests
+#import streamlit as st
 
-import streamlit as st
-from streamlit_lottie import st_lottie
 
 def load_lottieurl(url):
     r = requests.get(url)
@@ -20,7 +19,7 @@ st.title("Lalay's Blog")
 import streamlit as st
 
     # --- LOAD ASSETS ---
-lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20 fcfjwiyb.json")
+#lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20cfjwiyb.json")
 img_contact_form = Image.open("images/1.jpg")
 # --- PROJECTS ---
 with st.container():
